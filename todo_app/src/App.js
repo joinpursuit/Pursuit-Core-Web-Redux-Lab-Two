fc4a0015e0aa26fb5be39b634d15802e9f3073cd
Owen Jones <owenthefirst93@gmail.com>
2020-01-31 17:24:50 -0500

    working on todoList

---
+++ todo_app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AddTodo from './Components/AddTodo'
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+        <h1>TODO LIST </h1>
+        <AddTodo />
+    </div>
+  );
+}
+
+export default App;
