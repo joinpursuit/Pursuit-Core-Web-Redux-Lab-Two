0fd0c7412c30f62ec94fcab319dc1ccc94b293bf
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-07 13:03:52 -0500

    0.3.2, minor cleanup index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+/*
+Joseph P. Pasaoa
+INDEX | Todos a la Redux | React Redux Two Lab
+*/
 
-import store from './redux/store.js';
-import './index.css';
-import App from './App';
+
+    // external imports
+    import React from 'react';
+    import ReactDOM from 'react-dom';
+    import { Provider } from 'react-redux';
+
+    // local
+    import store from './redux/store.js';
+    import './index.css';
+
+    import App from './App';
+
 
 ReactDOM.render(
   <Provider store={store}>
