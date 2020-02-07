428c6f3665af8e9654e3b0b0cc7707af238b7204
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-07 13:06:02 -0500

    0.3.3, make AddForm component, todoReducer

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -5,20 +5,59 @@ AddForm Component | Todos a la Redux | React Redux Two Lab
 
 
     // external imports
-    import React from 'react';
-
-    // local imports
+    import React, { useState } from 'react';
+    import { connect } from 'react-redux';
+    
+    // local
+    import { ADD_TODO } from '../redux/actionTypes';
     // import './AddForm.css';
 
 
-const AddForm = () => {
+const AddForm = (props) => {
+
+  const [ todoTxt, setTodoTxt ] = useState("");
+  const [ errorMsg, setErrorMsg ] = useState("");
+
+  const refTodoTxt = React.createRef();
+
+
+  const handleChange = (e) => setTodoTxt(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const submission = todoTxt;
+    if (submission === "" || submission.trim() === "") {
+      setErrorMsg("Please enter a valid ToDo and submit again");
+    } else {
+      setErrorMsg("");
+      props.addTodo(submission);
+    }
+    refTodoTxt.current.focus();
+  }
+
 
   return (
-    <div>
-
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todoTxt} onChange={handleChange} name="todoTxt" placeholder="What needs doing?" ref={refTodoTxt} />
+        <button type="submit">Submit</button>
+      </form>
+      <p className="error-msg">{errorMsg}</p>
+    </>
   );
 }
 
 
-export default AddForm;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (todoTxt)=> {
+      dispatch({
+        type: ADD_TODO,
+        payload: todoTxt
+      })
+    }
+  }
+}
+
+
+export default connect(null, mapDispatchToProps)(AddForm);
