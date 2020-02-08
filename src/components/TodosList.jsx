813e0c0a569677ca513185d5105aa9250eeef434
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-08 02:57:41 -0500

    0.4.1, connect TodosList component to store

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -6,18 +6,23 @@ TodosList Component | Todos a la Redux | React Redux Two Lab
 
 // EXTERNALS - LOCALS
 import React from 'react';
+import { connect } from 'react-redux';
 
 // import './TodosList.css';
 
 
 // MAIN
-const TodosList = () => {
+const TodosList = ({ todos }) => {
+  let listTodos = null;
+  if (todos.length) {
+    listTodos = todos.map(todo => <li key={todo.id}>{todo.text}</li>);
+  }
   return (
-    <div>
-      TodosList component
-    </div>
+    <ul>
+      {listTodos}
+    </ul>
   );
 }
 
 
-export default TodosList;
+export default connect(state => state.todosState)(TodosList);
