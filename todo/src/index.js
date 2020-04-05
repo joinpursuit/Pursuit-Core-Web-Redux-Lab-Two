7a2d765ae8a4d0e6361f69890200dd71d3e31065
BrandonBrown <brandonbrown@pursuit.org>
2020-04-05 15:04:56 -0400

    install redux and wrap app in provider

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+
+import Store from './Store/Store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider Store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
