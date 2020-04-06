d9d28890c6f3140b46a6b90f16625a239e34e18e
Nilber Remon <nilberremon@gmail.com>
2020-04-06 13:47:44 -0400

    Add Functionality to TodoForm Component - Form Submit

--- redux-todo-lab/src/Components/TodoForm.js
+++ redux-todo-lab/src/Components/TodoForm.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 
 import Input from './Input'
 import {useInput} from '../Utilities/CustomHookery'
 
+
 const TodoForm = () => {
     
-    const [input, setInput] = useInput("")
+    const input = useInput("")
     
-    
+    const dispatch = useDispatch()
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        dispatch(input)
+        input.clearInput()
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <Input className="todoItem" type="text" name="todoItem" placeholder="Enter Todo" input={input}/>
-            <button type
+            <button type="submit">SUBMIT TODO</button>
         </form>
     )
 }
