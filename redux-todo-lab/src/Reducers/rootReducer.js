2988645a04e98724a71ef136eeaa4dcb4c081fa8
Nilber Remon <nilberremon@gmail.com>
2020-04-06 13:01:59 -0400

    Create React App, Install Redux Libraries, Create App File Structure

---
+++ redux-todo-lab/src/Reducers/rootReducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux' 
+
+import todoReducer from 
+
+const rootReducer = ({
+    id: idReducer,
+    todo: todoReducer,
+    filter: filterReducer
+    }
+)
+
+export default rootReducer
