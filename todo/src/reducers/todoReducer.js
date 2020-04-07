974d0894976330eacb50a63b88c922c0ec54547b
Kevin Brutus <kevinbrutus@pursuit.org>
2020-04-06 23:12:27 -0400

    cannot read property of undefined: completed

--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -1,6 +1,8 @@
 import { ADD_ITEM, IS_COMPLETE } from '../actions/actionTypes';
 
 const todoReducer = (state = [], action) => {
+    // const newState = [ ...state ];
+
     switch (action.type) {
         case ADD_ITEM:
             let todo = { 
@@ -8,7 +10,10 @@ const todoReducer = (state = [], action) => {
                 completed: false
             }
             return [todo, ...state]
+            // newState[todo.id] = todo; 
+            // return newState;
         case IS_COMPLETE:
+            // let completeItem = newState[action.payload]
             let completeItem = state[action.payload]
             completeItem.completed = !completeItem.completed
             return state
