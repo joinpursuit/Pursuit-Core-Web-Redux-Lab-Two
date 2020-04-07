7f76114cae87c7dd8994d00988d2bf48798a1730
maria liza <mariamartinez@pursuit.org>
2020-04-07 15:30:14 -0400

    work on addtodo reducer

---
+++ to_do_app/src/Actions/actions.js
@@ -0,0 +1,18 @@
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from "./actionTypes";
+
+let nextTodo = 0;
+
+export const addToDo = item => ({
+  type: ADD_TODO,
+  payload: { id: nextTodo++, item }
+});
+
+export const toggleToDo = id => ({
+  type: TOGGLE_TODO,
+  payload: { id }
+});
+
+export const setVisibilityFilter = filter => ({
+  type: SET_VISIBILITY_FILTER,
+  payload: { filter }
+});
---
+++ to_do_app/src/Reducers/todoReducer.js
@@ -0,0 +1,28 @@
+import { ADD_TODO, TOGGLE_TODO } from "../Actions/actionTypes";
+
+import { addTodo, toggleToDo } from "../Actions/actions";
+//do i need to import that action functions? 
+
+const initialState = {
+  id: 0,
+  text: "",
+  completed: false
+};
+
+const todoReducer = (state = initialState, action) => {
+    //reducers update application state by action types? 
+  Object.freeze(state);
+  const newState = { ...state };
+  //do i need to create a newState like this everytime?
+  switch (action.type) {
+    case ADD_TODO: 
+    //is this where i create what i want to come out of this reducer? like here i'm trying to update a 'todo' item as the case ADD_TODO is true? what does it really mean to update the application state?
+    let todo = {
+        id: action.payload.id,
+        text: action.payload.item,
+        completed: false
+    }
+  }
+};
+
+//not really sure what should be returned from the reducer.
